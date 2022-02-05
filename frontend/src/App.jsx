b2a6cf8d1ee418aaa8bc752f63dd11b8ae3b2ead
Sangsan <sangsan.prohmvitak@gmail.com>
2022-02-06 00:42:49 +0700

    Set proxy and fetch root api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,31 @@
+import React, { useEffect, useState } from "react";
 
 const App = () => {
+  const [message, setMessage] = useState("");
+  const getWelcomeMessage = async () => {
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const response = await fetch("/api", requestOptions);
+    const data = await response.json();
+
+    if (!response.ok) {
+    console.log("something messed up");
+    } else {
+    setMessage(data.message);
+    }
+  };
+
+  useEffect(() => {
+    getWelcomeMessage();
+  }, []);
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>{message}</h1>
     </div>
   );
 }
